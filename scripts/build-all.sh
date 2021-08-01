54ca79a973350b4b1af8a233934e160e8198ea60
TulaByte <83277885+tula-byte@users.noreply.github.com>
2021-08-01 18:50:20 +1000

    removed macOS build options

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -4,7 +4,6 @@ set -e
 cd "`dirname \"$0\"`"
 TOPDIR=$(pwd)
 
-PLATFORM_TARGET="macOS" ./build-libs.sh
 PLATFORM_TARGET="iOS" ./build-libs.sh
 ./build-xcframework.sh
 ./build-cpaproxy-xcframework.sh
