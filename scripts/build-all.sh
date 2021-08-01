ae5339ec8de6ecbe5418aa655e6b162b62bacedc
TulaByte <83277885+tula-byte@users.noreply.github.com>
2021-08-01 19:01:32 +1000

    reverted to original

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -4,6 +4,7 @@ set -e
 cd "`dirname \"$0\"`"
 TOPDIR=$(pwd)
 
+PLATFORM_TARGET="macOS" ./build-libs.sh
 PLATFORM_TARGET="iOS" ./build-libs.sh
 ./build-xcframework.sh
 ./build-cpaproxy-xcframework.sh
