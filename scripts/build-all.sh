cb8ddc0e8fbcae7ae1d9cc63e3cdb3d28c480fc4
TulaByte <83277885+tula-byte@users.noreply.github.com>
2021-08-01 19:11:47 +1000

    attempt with only arm64

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -4,7 +4,7 @@ set -e
 cd "`dirname \"$0\"`"
 TOPDIR=$(pwd)
 
-PLATFORM_TARGET="macOS" ./build-libs.sh
-PLATFORM_TARGET="iOS" ./build-libs.sh
+
+PLATFORM_TARGET="iOS" ARCHS="arm64" ./build-libs.sh
 ./build-xcframework.sh
 ./build-cpaproxy-xcframework.sh
