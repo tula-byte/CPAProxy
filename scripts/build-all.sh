0c0fa75e9de34c8e4929228e09234c053b62b622
TulaByte <83277885+tula-byte@users.noreply.github.com>
2021-08-02 16:23:20 +1000

    Update build-all.sh

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -4,7 +4,7 @@ set -e
 cd "`dirname \"$0\"`"
 TOPDIR=$(pwd)
 
-PLATFORM_TARGET="macOS" ARCHS="arm64" ./build-libs.sh "11.3"
 PLATFORM_TARGET="iOS" ARCHS="arm64" ./build-libs.sh "14.5"
+PLATFORM_TARGET="macOS" ARCHS="arm64" ./build-libs.sh "11.3"
 ARCHS="arm64" ./build-xcframework.sh
 ./build-cpaproxy-xcframework.sh
